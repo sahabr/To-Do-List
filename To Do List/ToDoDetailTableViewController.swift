46ff747c28081a71e9520a54c393d23f97c9a8f9
Brishti <you@example.com>
2021-02-28 14:53:09 -0500

    5.4-passing between controllers

--- To Do List/ToDoDetailTableViewController.swift
+++ To Do List/ToDoDetailTableViewController.swift
@@ -18,10 +18,12 @@ class ToDoDetailTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         nameField.text = toDoItem
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender:Any?){
+        toDoItem = nameField.text
+    }
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
         if isPresentingInAddMode {
--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -23,6 +23,22 @@ class ToDoListViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowDetail" {
             let destination = segue.destination as! ToDoDetailTableViewController
+            let selectedIndexPath = tableView.indexPathForSelectedRow!
+            destination.toDoItem = toDoArray[selectedIndexPath.row]
+        }
+    }
+    
+    @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
+        let source = segue.source as! ToDoDetailTableViewController
+        if let selectedIndexPath = tableView.indexPathForSelectedRow {
+            toDoArray[selectedIndexPath.row] = source.toDoItem
+            tableView.reloadRows(at: [selectedIndexPath], with: .automatic)
+
+        } else {
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(source.toDoItem)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+            tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
     }
 }
@@ -31,7 +47,6 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print("numbersOfRowsInSection \(toDoArray.count)")
         return toDoArray.count
-        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
